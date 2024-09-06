83fe9e6723846b3b27d1b5e909c8b15d5bde0883
Ruhan Shaikh <ruhan1192002@gmail.com>
2024-09-06 21:31:33 +0530

    mongoDb Campas

--- app/db/connectDb.js
+++ app/db/connectDb.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
+
 const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/share-a-sip`, {
-        useNewUrlParser: true,
-      });
-      console.log(`MongoDB Connected: {conn.connection.host}`);
-    } catch (error) {
-      console.error(error.message);
-      process.exit(1);
-    }
+  try {
+    // Use environment variable for MongoDB URI
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
-  export default connectDB;
+};
+
+export default connectDB;
